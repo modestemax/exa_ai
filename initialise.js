e10768cd58b98cc4a901469132876b19225a74b9
max <modestemax@gmail.com>
2018-02-28 04:18:54 +0100

    ...

--- initialise.js
+++ initialise.js
@@ -4,16 +4,24 @@ const tradejson = process.env.HOME + '/.trade.json';
 const apijson = process.env.HOME + '/.api.json';
 const amountjson = process.env.HOME + '/.amount.json';
 
-if (!fs.existsSync(tradejson)) {
-    fs.writeFileSync(tradejson, '{}');
-}
+[tradejson, amountjson].forEach(jsonfile => {
+
+    if (!fs.existsSync(jsonfile)) {
+        fs.writeFileSync(jsonfile, '{}');
+    } else {
+        try {
+            require(jsonfile)
+        } catch (ex) {
+            console.log(ex);
+            fs.writeFileSync(jsonfile, '{}');
+        }
+    }
+
+});
 
 if (!fs.existsSync(apijson)) {
     fs.writeFileSync(apijson, `{
   "api_key": "JUBT89CMW9nrw456m4axhgRG",
   "secret": "9KTyPjWTGp3finXDltUbYj09Vxu2rZBV"
 }`);
-}
-if (!fs.existsSync(amountjson)) {
-    fs.writeFileSync(amountjson, `{}`);
 }
